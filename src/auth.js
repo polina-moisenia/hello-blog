031504f48264f946af979226852ff6fc162039b8
p.moisenia <p.moysenia@gmail.com>
2019-07-14 11:55:47 +0300

    Updated from comments, questions on api remains

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const usersToCheck = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
 
 const printUsers = function(users){
-    users.forEach(user => console.log("Name - " + user.name + ", Email - " + user.login));
+    users.forEach(user => console.log(`Name - ${user.name}, Email - ${user.login}`));
 }
 
 console.log('Curent blog users :');
@@ -11,12 +11,14 @@ printUsers(usersToCheck);
 //Implementation
 class Auth {
     constructor(permisions) {
-        this.permisions = permisions
+        this.permisions = permisions;
     }
 
     canUser(rule, user){
-        const [ ruleFound ] = this.permisions.filter(permision => permision.rule === rule && user[permision.field].match(permision.match));
-        return ruleFound != null;
+        const [ ruleFound ] = this.permisions.filter(
+            permision => permision.rule === rule &&
+            user[permision.field].match(permision.match));
+        return ruleFound;
     }
 }
 
@@ -32,11 +34,30 @@ const auth = new Auth(authPermisions);
 //TODO add login/logout endpoints
 
 //TODO move checks to unit tests
+const usersView = [];
+const usersAdd = [];
+const usersDelete = [];
+
+for (i = 0; i < usersToCheck.length; i++) { 
+    const user = usersToCheck[i];
+    if(auth.canUser('VIEW_POSTS', user)){
+        usersView.push(user);
+    }
+
+    if(auth.canUser('ADD_POSTS', user)){
+        usersAdd.push(user);
+    }
+
+    if(auth.canUser('DELETE_POSTS', user)){
+        usersDelete.push(user);
+    }
+}
+
 console.log('Users that can view posts :');
-printUsers(usersToCheck.filter(user => auth.canUser('VIEW_POSTS', user)));
+printUsers(usersView);
 
 console.log('Users that can add posts :');
-printUsers(usersToCheck.filter(user => auth.canUser('ADD_POSTS', user)));
+printUsers(usersAdd);
 
 console.log('Users that can delete posts :');
-printUsers(usersToCheck.filter(user => auth.canUser('DELETE_POSTS', user)));
+printUsers(usersDelete);
