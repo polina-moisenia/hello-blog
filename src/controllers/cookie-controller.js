0cd6a5f237b6bbbe8a5876d8f7b1d74912536b17
p.moisenia <p.moysenia@gmail.com>
2019-07-27 20:36:46 +0300

    Http status added

--- src/controllers/cookie-controller.js
+++ src/controllers/cookie-controller.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const bcrypt = require('bcrypt');
 const path = require('path');
+const httpStatus = require('http-status');
 const auth = require(path.join(__dirname, '../utils/auth.js'));
 const { usersDataLocation } = require('../config.js');
 const usersCollection = JSON.parse(fs.readFileSync(usersDataLocation, 'utf8'));
@@ -10,22 +11,22 @@ const cookieName = 'logged-in';
 const setCookie = function (req, res) {
     const cookie = req.cookies[cookieName];
     if(cookie){
-        res.status(302).redirect('/posts');
+        res.status(httpStatus.PERMANENT_REDIRECT).redirect('/posts');
     } else {
         const login = req.body.login;
         const password = req.body.password;
         if (!login || !password) {
-            res.status(400).send('Bad request');
+            res.status(httpStatus.BAD_REQUEST).send('Login and password requiried');
         }
 
         const [userFound] = usersCollection.filter(user => user.login === login);
         if (!userFound) {
-            res.status(401).send('Wrong user, try /login again');
+            res.status(httpStatus.UNAUTHORIZED).send('Wrong user, try /login again');
         } else if (!checkPasswordForUser(userFound, password)) {
-            res.status(401).send('Wrong password, try /login again');
+            res.status(httpStatus.UNAUTHORIZED).send('Wrong password, try /login again');
         } else {
             res.cookie(cookieName, login);
-            res.status(302).redirect('/posts');
+            res.status(httpStatus.PERMANENT_REDIRECT).redirect('/posts');
         }
     }
 };
@@ -39,9 +40,9 @@ const deleteCookie = function (req, res) {
     const cookie = req.cookies[cookieName];
     if(cookie) {
         res.clearCookie(cookieName);
-        res.status(200).send('Logged out');
+        res.status(httpStatus.OK).send('Logged out');
     } else {
-        res.status(403).send('Already logged out');
+        res.status(httpStatus.FORBIDDEN).send('Already logged out');
     }
 };
 
@@ -54,7 +55,7 @@ const authorizeByCookie = function (rule) {
             const [userFound] = usersCollection.filter(user => user.login === login);
             auth.canUser(rule, userFound) ? next() : res.status(401).send('No permission to procced for this user, try /login with another one');
         } else{
-            res.status(401).send('User must be logged in, try /login');
+            res.status(httpStatus.UNAUTHORIZED).send('User must be logged in, try /login');
         }
     }
 };
--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,17 +1,13 @@
 const fs = require('fs');
-const path = require('path');
+const httpStatus = require('http-status');
 const bcrypt = require('bcrypt');
-const cron = require('cron');
 const { usersDataLocation } = require('../config.js');
 const { getGravatarProfile } = require('../utils/gravatar-loader.js');
 const saltRounds = 10;
 const usersCollection = JSON.parse(fs.readFileSync(usersDataLocation, 'utf8'));
 
 const getUsers = function (req, res) {
-  res.json(usersCollection.map(({ password, ...user }) => {
-    //user.avatarUrl = getAvatarURL(user.login);
-    return user;
-  }));
+  res.json(usersCollection.map(({ password, ...user }) => user));
 }
 
 const getUserById = async function (req, res) {
@@ -20,19 +16,19 @@ const getUserById = async function (req, res) {
     user.gravatarProfile = await getGravatarProfile(user.login);
     res.json(user);
   } else {
-    res.status(404).send('User was not found');
+    res.status(httpStatus.NOT_FOUND).send('User was not found');
   }
 };
 
 const updateUser = function (req, res) {
   const userUpdated = req.body;
   //TODO rewrite validation
-  if (!userUpdated || !userUpdated.name || !userUpdated.login || !userUpdated.password) res.status(400).send('Bad request');
+  if (!userUpdated || !userUpdated.name || !userUpdated.login || !userUpdated.password) res.status(httpStatus.BAD_REQUEST).send('Bad request');
 
   //TODO how users will be created, loaded?
   const index = usersCollection.findIndex(user => user.userId === req.params.id);
   if (index === -1) {
-    res.status(404).send('User was not found');
+    res.status(httpStatus.NOT_FOUND).send('User was not found');
   } else {
     userFound = usersCollection[index];
     userFound.name = userUpdated.name;
@@ -42,18 +38,18 @@ const updateUser = function (req, res) {
     usersCollection[index] = userFound;
     fs.writeFileSync(usersDataLocation, JSON.stringify(usersCollection));
 
-    res.status(200).send(`User ${userFound.userId} was updated`);
+    res.status(httpStatus.OK).send(`User ${userFound.userId} was updated`);
   }
 }
 
 const deleteUser = function (req, res) {
   const index = usersCollection.findIndex(user => user.userId === req.params.id);
   if (index === -1) {
-    res.status(404).send('User was not found');
+    res.status(httpStatus.NOT_FOUND).send('User was not found');
   } else {
     usersCollection.splice(index, 1);
     fs.writeFileSync(usersDataLocation, JSON.stringify(usersCollection));
-    res.status(200).send('User was deleted');
+    res.status(httpStatus.OK).send('User was deleted');
   }
 };
 
