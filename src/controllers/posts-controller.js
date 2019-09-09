5ee519d23eb1bdeebd2b82618bb6ea9e191641fa
pmoisenia <p.moysenia@gmail.com>
2019-09-09 18:28:22 +0300

    Logic on posts moved from controller

--- src/controllers/posts-controller.js
+++ src/controllers/posts-controller.js
@@ -1,57 +1,34 @@
-const fs = require('fs');
-const uuid = require('uuid');
 const httpStatus = require('http-status');
-const { postsDataLocation } = require('../config.js');
-const { createPostInCollection } = require('../utils/post-util.js');
-var postsCollection = JSON.parse(fs.readFileSync(postsDataLocation, 'utf8'));
+const { getAllPostsFromCollection, getPostFromCollectionById, createPostInCollection, updatePostInCollection, deletePostFromCollection} = require('../utils/post-util.js');
 
 const getPosts = function (req, res) {
-  res.json(JSON.parse(fs.readFileSync(postsDataLocation, 'utf8')));
+  res.json(getAllPostsFromCollection());
 };
 
 const createPost = function (req, res) {
   const post = req.body;
-  //TODO rewrite validation
   if (!post || !post.title || !post.summary) res.status(httpStatus.BAD_REQUEST).send('Bad request');
-
   createPostInCollection(post);
   res.status(httpStatus.CREATED).send(`Post was created, id = ${post.postId}`);
 };
 
 const getPostById = function (req, res) {
-  const post = postsCollection.find(post => post.postId === req.params.id);
+  const post = getPostFromCollectionById(req.params.id);
   post ? res.json(post) : res.status(httpStatus.NOT_FOUND).send('Post was not found');
 };
 
 const updatePost = function (req, res) {
   const postUpdated = req.body;
-  //TODO rewrite validation  
-  if (!postUpdated || !post.title || !post.summary) res.status(httpStatus.BAD_REQUEST).send('Bad request');
-
-  const index = postsCollection.findIndex(post => post.postId === req.params.id);
-  if (index === -1) {
-    res.status(httpStatus.NOT_FOUND).send('Post was not found');
-  } else {
-    //TODO add modified field to the schema if needed
-    postFound = postsCollection[index];
-    postFound.title = postUpdated.title;
-    postFound.summary = postUpdated.summary;
-
-    postsCollection[index] = postFound;
-    fs.writeFileSync(postsDataLocation, JSON.stringify(postsCollection));
-    res.status(httpStatus.OK).send(`Post ${postFound.postId} was updated`);
-  }
+  if (!postUpdated || !postUpdated.title || !postUpdated.summary) res.status(httpStatus.BAD_REQUEST).send('Bad request');
+  var post = updatePostInCollection(req.params.id, postUpdated);
+  if (!post) res.status(httpStatus.NOT_FOUND).send('Post was not found');
+  res.status(httpStatus.OK).send(`Post ${postFound.postId} was updated`);
 };
 
 const deletePost = function (req, res) {
-  const index = postsCollection.findIndex(post => post.postId === req.params.id);
-  if (index === -1) {
-    res.status(httpStatus.NOT_FOUND).send('Post was not found');
-  } else {
-    postsCollection.splice(index, 1);
-    fs.writeFileSync(postsDataLocation, JSON.stringify(postsCollection));
-    res.status(httpStatus.OK).send('Post was deleted');
-  }
+  const success = deletePostFromCollection(req.params.id);
+  if (success) res.status(httpStatus.OK).send('Post was deleted');
+  res.status(httpStatus.NOT_FOUND).send('Post was not found');
 };
 
 module.exports = {
--- src/utils/post-util.js
+++ src/utils/post-util.js
@@ -4,15 +4,52 @@ const uuid = require('uuid');
 const { postsDataLocation } = require('../config.js');
 const postsCollection = JSON.parse(fs.readFileSync(postsDataLocation, 'utf8'));
 
+const getAllPostsFromCollection = function () {
+    return postsCollection;
+}
+
+const getPostFromCollectionById = function (id) {
+    return postsCollection.find(post => post.postId === id);
+}
+
 const createPostInCollection = function (post) {
     post.postId = uuid.v4();
     post.createdAt = new Date().toISOString();
     postsCollection.push(post);
-    fs.writeFileSync(postsDataLocation, JSON.stringify(postsCollection)); 
-    
+    fs.writeFileSync(postsDataLocation, JSON.stringify(postsCollection));
     return post;
 };
 
+const updatePostInCollection = function (id, post) {
+    const index = postsCollection.findIndex(post => post.postId === id);
+    if (index === -1) {
+        return null;
+    } else {
+        postFound = postsCollection[index];
+        postFound.title = post.title;
+        postFound.summary = post.summary;
+
+        postsCollection[index] = postFound;
+        fs.writeFileSync(postsDataLocation, JSON.stringify(postsCollection));
+        return post;
+    }
+};
+
+const deletePostFromCollection = function (id) {
+    const index = postsCollection.findIndex(post => post.postId === id);
+    if (index === -1) {
+        return false;
+    } else {
+        postsCollection.splice(index, 1);
+        fs.writeFileSync(postsDataLocation, JSON.stringify(postsCollection));
+        return true;
+    }
+}
+
 module.exports = {
-    createPostInCollection
+    getAllPostsFromCollection,
+    getPostFromCollectionById,
+    createPostInCollection,
+    updatePostInCollection,
+    deletePostFromCollection
 }
